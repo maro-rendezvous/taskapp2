ac01d60df8af0a39ae59d354d596ef4329bebdd0
ochiai <ochiai.k@trend-art.net>
2021-05-30 20:30:31 +0900

    Initial Commit

---
+++ taskapp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskapp2
+//
+//  Created by 落合克彦 on 2021/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
